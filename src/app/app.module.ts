64bf24f37a1602cd5c32c994013a0c34369f8287
ramiro <ceinnova13458@innovaccion.mx>
2023-06-05 10:18:22 -0600

    Petición hacia endpoint de API realizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 
@@ -8,7 +9,7 @@ import { GiftsModule } from './gifts/gifts.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GiftsModule, SharedModule],
+  imports: [BrowserModule, HttpClientModule, GiftsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/gifts/services/gifts.service.ts
+++ src/app/gifts/services/gifts.service.ts
@@ -1,10 +1,13 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GiftService {
   private _tagsHistory: string[] = [];
+  private apiKey: string = '';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -21,8 +24,17 @@ export class GiftService {
     this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
-  public searchTag(tag: string): void {
+  searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, { params }).subscribe((resp) => {
+      console.log(resp);
+    });
   }
 }
