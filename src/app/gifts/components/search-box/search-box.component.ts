57216fb4ac0df568c357eff763ddb3483ae6c2f6
ramiro <ceinnova13458@innovaccion.mx>
2023-06-05 09:12:18 -0600

    Añadida lectura del buscador al dar entenr

--- src/app/gifts/components/search-box/search-box.component.ts
+++ src/app/gifts/components/search-box/search-box.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'gifts-search-box',
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifts..." />
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifts..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
   `,
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
   constructor() {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 }
